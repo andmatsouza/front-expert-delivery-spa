e141cf68a35b85bf88433fc7507b36874834e955
andmatsou <andmatsou@gmail.com>
2022-05-10 14:48:03 -0300

    feat: adicionado família de ícones

---
+++ components/icon-family/index.tsx
@@ -0,0 +1,6 @@
+export * from "./icon-fire";
+export * from "./icon-map";
+export * from "./icon-knife";
+export * from "./icon-motorcycle";
+export * from "./icon-store";
+export * from "./icon-wallet";
