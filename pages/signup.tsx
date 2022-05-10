db9c7d33915009728994eb80924366428d89b776
andmatsou <andmatsou@gmail.com>
2022-05-10 14:54:32 -0300

    feat: refatoração page login

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -12,12 +12,12 @@ const Signup: NextPage = () => {
         <div className='lg:basis-8/12 flex'>
           <div className='max-w-2xl w-4/5 m-auto'>
             <h1 className='font-bold text-4xl mb-3'>Criar a conta</h1>
-            <p className='text-slate-700 mb-14'>Por favor crie uma conta para continuar usando o nosso serviço. </p>
+            <p className='mb-14'>Por favor crie uma conta para continuar usando o nosso serviço. </p>
             <InputText label='Nome Completo' type='text' placeholder='Informe o seu nome' />
             <InputText label='Endereço de E-mail' type='email' placeholder='Informe o seu e-mail' />
             <InputText label='Senha' type='password' placeholder='Informe a senha' />
             <Button className='bg-indigo-600 w-full p-4 text-white text-xl'>Criar uma conta</Button>
-            <p className='mt-6 text-center text-slate-700 mb-14'>
+            <p className='mt-6 text-center mb-14'>
               Você já tem uma conta?{" "}
               <Link href='/login'>
                 <a className='mt-6 text-orange-600 font-bold hover:text-orange-900'>Acesse aqui.</a>
