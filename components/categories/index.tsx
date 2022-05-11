bb998b63c5208a118d2564063f8adac838bd519f
andmatsou <andmatsou@gmail.com>
2022-05-11 10:28:35 -0300

    feat: adicionado novas categorias, cores e alinhamento

---
+++ components/categories/index.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+//import Styles from './styles.module.css';
+import Link from "next/link";
+import { Category } from '../../components';
+
+
+export const Categories = () => {
+  const categories = [
+    {
+      name: "Popular",
+      legend: "+286 opções",
+      iconName: "Fire",
+      backgroundSection: "#FB6D3A"
+    },
+    {
+      name: "Entrega Rápida",
+      legend: "+1.843 opções",
+      iconName: "motorcycle",
+      backgroundSection: "#503E9D"
+    },
+    {
+      name: "Alta Classe",
+      legend: "+25 opções",
+      iconName: "wallet",
+      backgroundSection: "#FACD5D"
+    },
+    {
+      name: "Jantar",
+      legend: "+182 opções",
+      iconName: "knife",
+      backgroundSection: "#FB6D3A"
+    },
+    {
+      name: "Para retirar",
+      legend: "+3.548 opções",
+      iconName: "store",
+      backgroundSection: "#503E9D"
+    },
+    {
+      name: "No bairro",
+      legend: "+44 opções",
+      iconName: "map",
+      backgroundSection: "#FACD5D"
+    }
+  ];
+  return (
+    <div>
+      <div className="flex">
+        <h2 className="grow">Explorar Categorias</h2>
+        <Link href="/categories">
+          <a>Ver todas</a>
+        </Link>
+      </div>
+      <div className="flex gap-6 justify-between">
+        {categories.map((category) => (
+          <Category key={category.name} {...category} />
+        ))}
+      </div>
+
+    </div>
+  )
+}
