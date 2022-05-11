334c2aa90e20c8b738281bfb3ce18ee124900944
andmatsou <andmatsou@gmail.com>
2022-05-11 10:30:27 -0300

    feat: adicionado ajustes no componentes da categoria

--- components/category/index.tsx
+++ components/category/index.tsx
@@ -1,21 +1,28 @@
 import Styles from './styles.module.css';
 import { IconBase } from '../../components';
+import Link from 'next/link';
 
 type Props = {
   name: string;
   legend: string;
   iconName: string;
+  backgroundSection: string;
 }
 
 
-export const Category = ({ name, legend, iconName }: Props) => {
+export const Category = ({ name, legend, iconName, backgroundSection }: Props) => {
   return (
-    <div className={Styles.card}>
-      <div>
-        <IconBase iconName={iconName} />
-      </div>
-      <h3>{name}</h3>
-      <p>{legend}</p>
-    </div>
+    <Link href="/categoria">
+      <a className={Styles.card}>
+        <div
+          className={Styles.icon}
+          style={{ backgroundColor: backgroundSection }}
+        >
+          <IconBase iconName={iconName} color="white" />
+        </div>
+        <h3>{name}</h3>
+        <p>{legend}</p>
+      </a>
+    </Link>
   )
 }
