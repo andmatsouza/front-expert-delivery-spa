93a1bce786d8c041e4cc4e47b5a5b43b61960cc6
andmatsou <andmatsou@gmail.com>
2022-05-12 18:24:32 -0300

    feat: refatoração inclusão id

--- components/categories/index.tsx
+++ components/categories/index.tsx
@@ -8,36 +8,42 @@ import { IconArrowRight } from "../icon-family";
 export const Categories = () => {
   const categories = [
     {
+      id: "1",
       name: "Popular",
       legend: "+286 opções",
       iconName: "Fire",
       backgroundSection: "#FB6D3A"
     },
     {
+      id: "2",
       name: "Entrega Rápida",
       legend: "+1.843 opções",
       iconName: "motorcycle",
       backgroundSection: "#503E9D"
     },
     {
+      id: "3",
       name: "Alta Classe",
       legend: "+25 opções",
       iconName: "wallet",
       backgroundSection: "#FACD5D"
     },
     {
+      id: "4",
       name: "Jantar",
       legend: "+182 opções",
       iconName: "knife",
       backgroundSection: "#FB6D3A"
     },
     {
+      id: "5",
       name: "Para retirar",
       legend: "+3.548 opções",
       iconName: "store",
       backgroundSection: "#503E9D"
     },
     {
+      id: "6",
       name: "No bairro",
       legend: "+44 opções",
       iconName: "map",
