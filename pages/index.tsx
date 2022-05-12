18711a03902c3b42477d138970304c8cb61c22ab
andmatsou <andmatsou@gmail.com>
2022-05-12 14:33:04 -0300

    feat: adicionado importação do componente de pratos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { Categories, Highlights, Navbar, Sidebar } from '../components';
+import { Categories, Highlights, Navbar, Sidebar, FoodList } from '../components';
 
 const Home: NextPage = () => {
   return (
@@ -16,6 +16,7 @@ const Home: NextPage = () => {
           <Navbar />
           <Categories />
           <Highlights />
+          <FoodList />
         </div>
       </div>
     </>
