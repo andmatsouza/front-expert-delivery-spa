e22afe2610c298bf01e191c2e7e10705d429e3f0
andmatsou <andmatsou@gmail.com>
2022-05-16 15:04:31 -0300

    feat: refatoração página home inclusão page template

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,16 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import { Categories, Highlights, Navbar, Sidebar, FoodList } from '../components';
+import type { NextPage } from 'next';
+import { Categories, Highlights, PageTemplate, FoodList } from '../components';
 
 const Home: NextPage = () => {
   return (
-    <>
-      <Head>
-        <title>Nibble Restaurantes</title>
-        <meta name="description" content="Descrição do Nibble Restaurantes" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className='container m-auto flex'>
-        <Sidebar />
-        <div className='grow p-8'>
-          <Navbar />
-          <Categories />
-          <Highlights />
-          <FoodList />
-        </div>
-      </div>
-    </>
+    <PageTemplate
+      title='Nibble Restaurantes'
+      description='Descrição do Nibble Restaurantes'
+    >
+      <Categories />
+      <Highlights />
+      <FoodList />
+    </PageTemplate>
   )
 }
 
