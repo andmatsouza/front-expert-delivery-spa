28a11e43a900d8f5282af260b14633af15edd9b4
andmatsou <andmatsou@gmail.com>
2022-05-17 19:48:15 -0300

    feat: refatoração da página restaurante e ajuste no layout

--- pages/restaurants/[id].tsx
+++ pages/restaurants/[id].tsx
@@ -1,4 +1,5 @@
-import { FoodList, PageTemplate, DetailsItem } from "../../components";
+import Image from "next/image";
+import { FoodList, PageTemplate, DetailsItem, Navbar, IconBase } from "../../components";
 import Styles from './restaurant.module.css';
 import { TypePageProps, TypeParams } from './types';
 
@@ -10,19 +11,36 @@ function Restaurant({ id, children }: TypePageProps) {
       description='Restaurante Burguer King'
       keywords='Restaurantes, BurguerKing, Burguers'
     >
-      <div>{id}</div>
       <div className={Styles.header}>
+        <div className={Styles.cover}>
+          <div className={Styles.image}>
+            <Image src="/cover.jpg" layout="fill" alt="Capa" />
+          </div>
+          <div className={Styles.brand}>
+            <Image
+              src="/brands/burguer-king.svg"
+              width={96}
+              height={96}
+              alt="Capa"
+            />
+          </div>
+          <button className={Styles.favouriteButton}>
+            <IconBase iconName="hearth" size={20} color="#000" />
+          </button>
+        </div>
         <div className={Styles.details}>
-          <h1>Burger King</h1>
+          <div className="flex flex-col md:flex-row mb-3">
+            <h1>Burger King</h1>
+            <div className={Styles.tags}>
+              <span>Entrega grátis</span>
+              <span>Compre 1 e leve 2</span>
+              <span>Abre às 12h</span>
+            </div>
+          </div>
           <p>
             Un dos maiores ícone e reconhecido restaurante de comida
             rápida no qual oferece realmente comidas e bebidas incríveis.
           </p>
-        </div>
-        <div className={Styles.tags}>
-          <span>Entrega grátis</span>
-          <span>Compre 1 e leve 2</span>
-          <span>Abre às 12h</span>
         </div>
       </div>
       <div className='mb-12'>
