1087b2e41e0d2dd1564135ef6d18955d0037d87a
andmatsou <andmatsou@gmail.com>
2022-05-25 13:25:59 -0300

    feat: atualização do componente perfil do usuario e dos estilos

--- components/user-profile/index.tsx
+++ components/user-profile/index.tsx
@@ -3,11 +3,11 @@ import Styles from "./styles.module.css";
 import Image from "next/image";
 
 export const UserProfile = () => {
-  return(
+  return (
     <div className={Styles.userProfile}>
       <div>
         <Image src="/user-avatar.png" height={48} width={48} alt="Usuário" />
-        </div>
+      </div>
       <div className={Styles.userData}>
         <h3 className={Styles.userData}>Mark Clarke</h3>
         <p className={Styles.userData}>markeclarke@gmail.com</p>
